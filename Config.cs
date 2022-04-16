3f80c1c76e206f146a88b0c0b77b652744a9f8d3
CarJem Generations (Carter Wallace) <cwallacecs@gmail.com>
2022-04-16 13:30:21 -0400

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockLauncher.Localization
+{
+    public static class Config
+    {
+        public static bool InstallerMode { get; set; } = false;
+        public static string LangFolder => InstallerMode ? "lang_installer" : "lang";
+    }
+}
---
+++ Language/AvaliableLanuages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockLauncher.Localization.Language
+{
+    public static class AvaliableLanuages
+    {
+        public static List<string> GetAll()
+        {
+            return new List<string>()
+            {
+                "en-US",
+                "fr-FR",
+                "fi-FI",
+                "ru-RU",
+                "pt-BR",
+                "es-ES",
+                "uk-UA"
+            };
+        }
+    }
+}
